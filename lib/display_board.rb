512d3b3685184bf0cd103212f3d340f8d33f87f7
Charity Youngblood <youngblood.charity@gmail.com>
2017-10-03 19:26:26 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,7 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board
   $cell =  "   "
-  $vertical_lines = "|  |"
+  $vertical_lines = "|"
   $horizontal_lines = "-----------"
       2.times do
       puts $cell + $vertical_lines + $cell
