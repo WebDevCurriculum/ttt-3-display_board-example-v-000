aa26f350e226293ad3b4b7928784bbbc3529bc24
Charity Youngblood <youngblood.charity@gmail.com>
2017-10-03 19:25:09 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,10 +3,10 @@ def display_board
   $cell =  "   "
   $vertical_lines = "|  |"
   $horizontal_lines = "-----------"
-      2.times do 
+      2.times do
       puts $cell + $vertical_lines + $cell
       puts $horizontal_lines
-      end 
+      end
       puts $cell + $vertical_lines + $cell
   end
 
